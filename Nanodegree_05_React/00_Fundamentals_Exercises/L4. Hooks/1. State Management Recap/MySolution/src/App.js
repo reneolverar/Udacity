9da0075c90c217360ddbb18f25a83146c2004224
Rene Olvera Romero <reneolverar@gmail.com>
2023-08-20 16:20:05 +0200

    Final commit

--- Nanodegree_05_React/00_Fundamentals_Exercises/L4. Hooks/1. State Management Recap/MySolution/src/App.js
+++ Nanodegree_05_React/00_Fundamentals_Exercises/L4. Hooks/1. State Management Recap/MySolution/src/App.js
@@ -1,19 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ChatWindow from "./ChatWindow";
+import { useState } from "react";
 
 const users = [{ username: "Amy" }, { username: "John" }];
 
-const messages = [
-  { username: "Amy", text: "Hi, Jon!" },
-  { username: "Amy", text: "How are you?" },
-  { username: "John", text: "Hi, Amy! Good, you?" },
-];
-
 const App = () => {
-  // If the user did not type anything, he/she should not be allowed to submit.
-  const isDisabled = () => {
-    return false;
-  };
+
+  const [messages, setMessages] = useState([
+    { username: "Amy", text: "Hi, Jon!" },
+    { username: "Amy", text: "How are you?" },
+    { username: "John", text: "Hi, Amy! Good, you?" },
+  ]);
+
+  const addMessage = (username, message) => {
+    const newMessage = { username: username, text: message }
+    setMessages([...messages, newMessage])
+  }
 
   return (
     <div className="App">
@@ -22,74 +25,15 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[0].username}</div>
+        {users.map((user) => (
+          <ChatWindow
+            key={user.username}
+            user={user}
+            messages={messages}
+            onAddMessage={addMessage}
+          />
+        ))}
 
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[0].username
-                    ? "message sender"
-                    : "message recipient"
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your message..."
-              />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled()}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[1].username}</div>
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[1].username
-                    ? "message sender"
-                    : "message recipient"
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your message..."
-              />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled()}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
       </div>
     </div>
   );
