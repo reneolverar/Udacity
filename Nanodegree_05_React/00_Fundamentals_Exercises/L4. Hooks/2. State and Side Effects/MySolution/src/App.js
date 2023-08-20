93ec80317d0babc6cab29d78b39151d521de94f2
Rene Olvera Romero <reneolverar@gmail.com>
2023-08-20 15:04:50 +0200

    Question about infinite recursion

--- Nanodegree_05_React/00_Fundamentals_Exercises/L4. Hooks/2. State and Side Effects/MySolution/src/App.js
+++ Nanodegree_05_React/00_Fundamentals_Exercises/L4. Hooks/2. State and Side Effects/MySolution/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Score from "./Score";
+import Game from "./Game";
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
 const numQuestions = 0;
 const numCorrect = 0;
+
+const handleAnswer = (answer) => {
+
+}
 
 const App = () => {
   return (
@@ -17,14 +19,8 @@ const App = () => {
       </header>
       <div className="game">
         <h2>Mental Math</h2>
-        <div className="equation">
-          <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-        </div>
-        <button>True</button>
-        <button>False</button>
-        <p className="text">
-          Your Score: {numCorrect}/{numQuestions}
-        </p>
+        <Game handleAnswer={handleAnswer} />
+        <Score numCorrect={numCorrect} numQuestions={numQuestions}/>
       </div>
     </div>
   );
---
+++ Nanodegree_05_React/00_Fundamentals_Exercises/L4. Hooks/2. State and Side Effects/MySolution/src/Game.js
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+
+const Game = ({ handleAnswer }) => {
+
+    const [gameValues, setGameValues] = useState({})
+
+      useEffect(() => {
+          makeNewGame()
+      }, []);
+
+    const makeNewGame = () => {
+        setGameValues(makeNewQuestion())
+    }
+
+    const makeNewQuestion = () => {
+        let newQuestion = {
+            value1: Math.floor(Math.random() * 100),
+            value2: Math.floor(Math.random() * 100),
+            value3: Math.floor(Math.random() * 100),
+        }
+        newQuestion.proposedAnswer = Math.floor(Math.random() * 3) + newQuestion.value1 + newQuestion.value2 + newQuestion.value3
+        return newQuestion
+    }
+
+    // const handleInput = (event) => {
+    //     const correctAnswer = gameValues.proposedAnswer === gameValues.value1 + gameValues.value2 + gameValues.value3
+    //     if (correctAnswer === eval(event.target.name)) {
+    //         handleAnswer(true)
+    //     } else {
+    //         handleAnswer(false)
+    //     }
+    //     makeNewGame();
+    // }
+
+     const handleInput = (userAnswer) => {
+        const correctAnswer = gameValues.proposedAnswer === gameValues.value1 + gameValues.value2 + gameValues.value3
+        if (correctAnswer === userAnswer) {
+            handleAnswer(true)
+        } else {
+            handleAnswer(false)
+        }
+        makeNewGame();
+    }
+
+  return (
+       <div>
+            <div className="equation">
+            <p className="text">{`${gameValues.value1} + ${gameValues.value2} + ${gameValues.value3} = ${gameValues.proposedAnswer}`}</p>
+            </div>
+            {/* <button name="true" onClick={handleInput}>True</button>
+            <button name="false" onClick={handleInput}>False</button> */}
+            <button name="true" onClick={handleInput(true)}>True</button>
+            <button name="false" onClick={handleInput(false)}>False</button>
+        </div>
+  );
+};
+
+Game.propTypes = {
+    handleAnswer: PropTypes.func.isRequired,
+};
+
+export default Game;
