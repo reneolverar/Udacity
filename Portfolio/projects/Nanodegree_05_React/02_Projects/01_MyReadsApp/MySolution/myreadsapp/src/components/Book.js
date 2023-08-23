79723ea1dad7e6c545e25e2486c1f97210ab460a
Rene Olvera Romero <reneolverar@gmail.com>
2023-08-23 18:03:07 +0200

    feat: Book component initial commit

---
+++ Portfolio/projects/Nanodegree_05_React/02_Projects/01_MyReadsApp/MySolution/myreadsapp/src/components/Book.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ShelfChanger from './ShelfChanger'
+
+function Book(props) {
+    const { title, authors, imageLinks, shelf } = props.book
+    const onShelfChange = props.onShelfChange
+    return (
+        <div className="book">
+            <div className="book-top">
+            <div
+                className="book-cover"
+                style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url("${imageLinks.smallThumbnail}")`,
+                }}
+            ></div>
+                <ShelfChanger
+                    shelf={shelf}
+                    book={props.book}
+                    onShelfChange={onShelfChange}
+                />
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors}</div>
+        </div>
+    )
+}
+
+Book.propTypes = {
+    book: PropTypes.array.isRequired,
+    onShelfChange: PropTypes.func.isRequired,
+}
+
+export default Book
