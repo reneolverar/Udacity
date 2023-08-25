3b4146c437ba38ac15709e6f9877351b8daab366
Rene Olvera Romero <reneolverar@gmail.com>
2023-08-25 16:16:14 +0200

    feat: Use lodash/debounce to avoid spamming the server when typing

--- Portfolio/projects/Nanodegree_05_React/02_Projects/01_MyReadsApp/MySolution/src/components/SearchBookInputField.js
+++ Portfolio/projects/Nanodegree_05_React/02_Projects/01_MyReadsApp/MySolution/src/components/SearchBookInputField.js
@@ -1,35 +1,32 @@
-import { useState } from "react";
-import PropTypes from 'prop-types'
-import _ from lodash
+import { useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import debounce from "lodash/debounce";
 
+export default function BookSearchBox({ onSearchInputChange }) {
+  const [query, setQuery] = useState("");
 
-export default function BookSearchBox({onSearchInputChange}) {
+  const handleInputChange = (query) => {
+    setQuery(query);
+    debouncedSearch(query);
+  };
 
-    const [query, setQuery] = useState("")
-
-
-
-    const handleInputChange = (query) => {
-        setQuery(query)
-        _.debounce(onSearchInputChange(query))
-    }
-
-
+    const debouncedSearch = useCallback(
+        debounce((query) => onSearchInputChange(query), 600),
+        []
+    )
 
   return (
     <div className="search-books-input-wrapper">
-        <input
-            value={query}
-            onChange={(event) => handleInputChange(event.target.value)}
-            type="text"
-            placeholder="Search by title, author, or ISBN"
-        />
+      <input
+        value={query}
+        onChange={(event) => handleInputChange(event.target.value)}
+        type="text"
+        placeholder="Search by title, author, or ISBN"
+      />
     </div>
-  )
+  );
 }
 
 BookSearchBox.propTypes = {
-    onSearchInputChange: PropTypes.func.isRequired,
-}
-
-
+  onSearchInputChange: PropTypes.func.isRequired,
+};
